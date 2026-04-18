1ec04ef5f6c6431de10a6a8506bb2f99d44bd013
mint1524 <hutr8276@yandex.ru>
2026-04-18 13:20:28 +0300

    split app config into plugin files, add /health

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,13 +1,18 @@
 package com.example
 
-import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
+import io.ktor.server.application.Application
+import io.ktor.server.response.respond
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class HealthResponse(val status: String)
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello, World!")
+        get("/health") {
+            call.respond(HealthResponse("ok"))
         }
     }
 }
