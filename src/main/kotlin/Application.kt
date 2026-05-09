8ae6b935e4f9cff566e5fd63dfc8f8e977a542b8
mint1524 <mint1524@icloud.com>
2026-05-09 03:51:42 +0300

    auth routes: register and login

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,11 +1,13 @@
 package com.example
 
+import com.example.auth.TokenService
 import com.example.config.loadConfig
 import com.example.plugins.configureCors
 import com.example.plugins.configureMonitoring
 import com.example.plugins.configureSecurity
 import com.example.plugins.configureSerialization
 import com.example.plugins.configureStatusPages
+import com.example.users.InMemoryUserRepository
 import io.ktor.server.application.Application
 import io.ktor.server.application.log
 
@@ -13,10 +15,13 @@ fun Application.module() {
     val config = loadConfig()
     log.info("starting okak backend, jwt issuer=${config.jwt.issuer}")
 
+    val users = InMemoryUserRepository()
+    val tokens = TokenService(config.jwt)
+
     configureMonitoring()
     configureSerialization()
     configureStatusPages()
     configureCors()
     configureSecurity(config.jwt)
-    configureRouting()
+    configureRouting(users, tokens)
 }
